a98c94461448147c75db24ffa4ed4a9b1d34fa11
Chuck Zhou <chuckzhb@hotmail.com>
2020-06-26 13:23:52 +0800

    1) Test how to response with json format with some small json file or str in GCDWebServer. 2) It turns out that [GCDWebServerDataResponse responseWithJSONObject:] requires Dictionary data type but not Data type. 3) make a small test, now server could response json data successfully. 4) Next: figure out how to determine the query string and return the json data accorrdingly.

--- DySearchAPI/ViewController.swift
+++ DySearchAPI/ViewController.swift
@@ -7,16 +7,75 @@
 //
 
 import UIKit
+import GCDWebServer
 
 class ViewController: UIViewController {
+    
+    lazy var webServer = GCDWebServer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initWebServer()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        stopWebServer()
+    }
     
+    func initWebServer() {
+        
+        let jsonData = NSData(contentsOfFile: "mock-data.json")
+        var jsonDict = [String: Any]()
+        
+        let str = "{\"names\": [\"Bob\", \"Tim\", \"Tina\"]}"
+        let data = Data(str.utf8)
 
+        do {
+            // make sure this JSON is in the format we expect
+            if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                // try to read out a string array
+                jsonDict = json
+                
+                if let names = json["names"] as? [String] {
+                    print("HAIBO: ", names)
+                }
+            }
+        } catch let error as NSError {
+            print("Failed to load: \(error.localizedDescription)")
+        }
+//        do {
+//            if let jsonDict = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [[String: Any]] {
+//                if let brandName = jsonDict["brand_name"] as? [String] {
+//                    print(brandName)
+//                }
+//            }
+//        } catch let error as NSError {
+//            print("Failed to load: \(error.localizedDescription)")
+//        }
+        
+        let path = "/search/brand"
+        webServer.addHandler(forMethod: "GET", path: path, request: GCDWebServerRequest.self, processBlock: { request in
+            print("WebServer - GET detected")
+            return GCDWebServerDataResponse(jsonObject: jsonDict)
+        })
+        
+        
+        webServer.start(withPort: 8080, bonjourName: "My Test Site")
+        
+        DispatchQueue.main.async {
+            if let serverURL = self.webServer.serverURL {
+                print("VISIT \(serverURL) in your web browser")
+            } else {
+                print("Not connected!")
+            }
+        }
+    }
+    
+    func stopWebServer() {
+        webServer.stop()
+    }
 
 }
 
